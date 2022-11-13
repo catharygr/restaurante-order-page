f25ef6a8e7f4dea1b4be08767fd87ce0cfe21a21
cathy <cathy@bubulazi.com>
2022-11-13 15:33:28 +0000

    Implementacion del ticket terminado

--- script/data.js
+++ script/data.js
@@ -1,23 +1,30 @@
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
+
+
+
 export const menuArray = [
   {
       name: "Pizza",
       ingredients: ["pepperoni", "mushrom", "mozarella"],
-      id: 0,
       price: 14,
-      emoji: "🍕"
+      emoji: "🍕",
+      type: "food",
+      id: uuidv4()
   },
   {
       name: "Hamburger",
       ingredients: ["beef", "cheese", "lettuce"],
       price: 12,
       emoji: "🍔",
-      id: 1
+      type: "food",
+      id: uuidv4()
   },
       {
       name: "Beer",
       ingredients: ["grain, hops, yeast, water"],
       price: 12,
       emoji: "🍺",
-      id: 2
+      type: "drink",
+      id: uuidv4()
   }
 ]
--- script/index.js
+++ script/index.js
@@ -1,5 +1,28 @@
 import {menuArray as dataArray} from "./data.js"
 
+const ticketArray = []
+let descuento = false
+
+
+document.addEventListener('click', function(e) {
+  if(e.target.dataset.agregarticket) {
+    AgregarProducto(e.target.dataset.agregarticket)
+  }
+  if(e.target.dataset.btnremover) {
+    ticketArray > 0 && removerProducto(e.target.dataset.btnremover)
+  }
+})
+
+ function AgregarProducto(uuidProducto) {
+  ticketArray.push(dataArray.find(producto => producto.id === uuidProducto))
+  renderTicket(ticketArray)
+ }
+
+ function removerProducto(uuidProducto) {
+  const indice = ticketArray.findIndex(producto => producto.id === uuidProducto)
+  ticketArray.splice(indice,1)
+  renderTicket()
+}
 
 
 
@@ -21,7 +44,7 @@ function getProductosHtml(data) {
           <p>${ingredientsHtml}</p>
           <p class="precio">${producto.price}</p>
         </div>
-        <button class="add-btn">+</button>
+        <button data-agregarticket="${producto.id}" class="add-btn">+</button>
       </div>
     ` 
   });
@@ -31,13 +54,82 @@ return html
 }
 
 
+function getTicketHtml(data) {
+  const ticketActual = {
+    precio: 0,
+    tipoProducto: [],
+    cantidadDescuento: 0,
+    precioTotal: 0
+  }
+  
+  let listadoHtml = []
+
+  ticketArray.forEach( producto => {
+    ticketActual.precio += producto.price
+    ticketActual.tipoProducto.push(producto.type)
+    if( 
+      ticketActual.tipoProducto.some( tipo => tipo === 'food') &&
+      ticketActual.tipoProducto.some( tipo => tipo ===  'drink') 
+      ) {
+      descuento = true
+      ticketActual.cantidadDescuento = (ticketActual.precio / 100 * 10).toFixed(2)
+      ticketActual.precioTotal = ticketActual.precio - ticketActual.cantidadDescuento
+     
+    } else {
+      descuento = false
+      ticketActual.precioTotal = ticketActual.precio
+    }
+   listadoHtml += `
+        <div class="producto-unico">
+          <h3>${producto.name}</h3>
+          <button data-btnremover="${producto.id}" class="btn-quitar">Remover</button>
+          <p>${producto.price}</p>
+        </div>
+   `
+   
+  } )
+  const html = `
+      <h3>Su pedido</h3>
+      
+      <div id="producto-listado">
+        ${listadoHtml}
+      </div>
+
+        <div class="totales">
+          <div class="totales-elemento">
+            <h3>Precio:</h3>
+            <p>${ticketActual.precio}</p>
+          </div>
+          <div class="totales-elemento">
+            <p id="des-disponible" class="no-disponible">Descuento combo: <span>10%</span></p>
+            <p>${ticketActual.cantidadDescuento}</p>
+          </div>
+          <div class="totales-elemento">
+            <h3>Precio Total:</h3>
+            <p>${ticketActual.precioTotal}</p>
+          </div>
+          <button class="btn-pedido">Completar pedido</button>
+      </div>
+  `
+  return html
+}
 
 
 
 
-
-function render() {
+function renderProductos() {
   document.querySelector('#productos').innerHTML = getProductosHtml(dataArray)
 }
 
-render()
+renderProductos()
+
+
+function renderTicket() {
+  document.querySelector('#ticket').innerHTML = getTicketHtml(ticketArray)
+  const descuentoParrafo = document.querySelector('#des-disponible')
+
+if(descuento) {
+  descuentoParrafo.classList.remove('no-disponible')
+} else if(!descuento)
+descuentoParrafo.classList.add('no-disponible')
+}
