da6c27df6635a4fecb32ecd0c339eff4eaf90364
cathy <cathy@bubulazi.com>
2022-11-15 13:23:35 +0000

    Terminado la mini aplicación del restaurante Guantanamera

--- script/data.js
+++ script/data.js
@@ -1,30 +1,26 @@
-import { v4 as uuidv4 } from "https://jspm.dev/uuid";
-
-
-
 export const menuArray = [
-  {
-      name: "Pizza",
-      ingredients: ["pepperoni", "mushrom", "mozarella"],
-      price: 14,
-      emoji: "🍕",
-      type: "food",
-      id: uuidv4()
-  },
-  {
-      name: "Hamburger",
-      ingredients: ["beef", "cheese", "lettuce"],
-      price: 12,
-      emoji: "🍔",
-      type: "food",
-      id: uuidv4()
-  },
-      {
-      name: "Beer",
-      ingredients: ["grain, hops, yeast, water"],
-      price: 12,
-      emoji: "🍺",
-      type: "drink",
-      id: uuidv4()
-  }
+    {
+        name: "Pizza",
+        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        price: 14,
+        emoji: "🍕",
+        type: "food",
+        id: "279a932f-9185-4482-ad3a-f06da6293064"
+    },
+    {
+        name: "Hamburger",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        emoji: "🍔",
+        type: "food",
+        id: "a58b2be2-cd95-4886-9b2c-4cd320286adb"
+    },
+    {
+        name: "Beer",
+        ingredients: ["grain, hops, yeast, water"],
+        price: 12,
+        emoji: "🍺",
+        type: "drink",
+        id: "371b8148-da2c-4fa9-b255-3959566b8a54"
+    }
 ]
--- script/index.js
+++ script/index.js
@@ -1,39 +1,62 @@
-import {menuArray as dataArray} from "./data.js"
+import { menuArray as dataArray } from "./data.js";
 
-const ticketArray = []
-let descuento = false
+const dialogo = document.querySelector("#dialogo");
+const modal = document.querySelector("#modal");
 
+const ticketArray = [];
+let descuento = false;
 
-document.addEventListener('click', function(e) {
-  if(e.target.dataset.agregarticket) {
-    AgregarProducto(e.target.dataset.agregarticket)
+document.addEventListener("click", function (e) {
+  if (e.target.dataset.agregarticket) {
+    AgregarProducto(e.target.dataset.agregarticket);
   }
-  if(e.target.dataset.btnremover) {
-    ticketArray > 0 && removerProducto(e.target.dataset.btnremover)
+  if (e.target.dataset.btnremover) {
+    ticketArray.length > 0 && removerProducto(e.target.dataset.btnremover);
   }
-})
+  if (e.target.id === "btn-pedido") {
+    dialogo.showModal();
+  }
+  if (e.target.id === "btn-pagar") {
+    terminarpago();
+  }
+});
 
- function AgregarProducto(uuidProducto) {
-  ticketArray.push(dataArray.find(producto => producto.id === uuidProducto))
-  renderTicket(ticketArray)
- }
-
- function removerProducto(uuidProducto) {
-  const indice = ticketArray.findIndex(producto => producto.id === uuidProducto)
-  ticketArray.splice(indice,1)
-  renderTicket()
+function terminarpago() {
+  ticketArray.length = 0;
+  document.querySelector("#ticket").innerHTML = `
+  <div class="pago-exito-msg">
+      <h2>¡Gracias, ${modal.value} por tu pedido, ya va en camino!</h2>
+  </div>
+  `;
+  dialogo.close();
+  const input = document.querySelectorAll("input");
+  input.forEach((input) => {
+    input.value = "";
+  });
 }
 
+function AgregarProducto(uuidProducto) {
+  ticketArray.push(dataArray.find((producto) => producto.id === uuidProducto));
+  renderTicket(ticketArray);
+}
 
+function removerProducto(uuidProducto) {
+  const indice = ticketArray.findIndex(
+    (producto) => producto.id === uuidProducto
+  );
+  ticketArray.splice(indice, 1);
+  renderTicket();
+}
 
 function getProductosHtml(data) {
-  let html = ''
+  let html = "";
 
-  data.forEach(producto => {
-    let ingredientsHtml = ''
-    producto.ingredients.forEach(ingr => {
-      ingredientsHtml += `${ingr}, `
-    })
+  data.forEach((producto) => {
+    let ingredientsHtml = "";
+    producto.ingredients.forEach((ingr) => {
+      ingredientsHtml += `${ingr}, `;
+    });
+    ingredientsHtml = ingredientsHtml.slice(0, -2);
     html += `
       <div class="producto">
         <div>
@@ -46,48 +69,48 @@ function getProductosHtml(data) {
         </div>
         <button data-agregarticket="${producto.id}" class="add-btn">+</button>
       </div>
-    ` 
+    `;
   });
 
-return html
-
+  return html;
 }
-
 
 function getTicketHtml(data) {
   const ticketActual = {
     precio: 0,
     tipoProducto: [],
     cantidadDescuento: 0,
-    precioTotal: 0
-  }
-  
-  let listadoHtml = []
+    precioTotal: 0,
+  };
 
-  ticketArray.forEach( producto => {
-    ticketActual.precio += producto.price
-    ticketActual.tipoProducto.push(producto.type)
-    if( 
-      ticketActual.tipoProducto.some( tipo => tipo === 'food') &&
-      ticketActual.tipoProducto.some( tipo => tipo ===  'drink') 
-      ) {
-      descuento = true
-      ticketActual.cantidadDescuento = (ticketActual.precio / 100 * 10).toFixed(2)
-      ticketActual.precioTotal = ticketActual.precio - ticketActual.cantidadDescuento
-     
+  let listadoHtml = [];
+
+  ticketArray.forEach((producto) => {
+    ticketActual.precio += producto.price;
+    ticketActual.tipoProducto.push(producto.type);
+    if (
+      ticketActual.tipoProducto.some((tipo) => tipo === "food") &&
+      ticketActual.tipoProducto.some((tipo) => tipo === "drink")
+    ) {
+      descuento = true;
+      ticketActual.cantidadDescuento = (
+        (ticketActual.precio / 100) *
+        10
+      ).toFixed(2);
+      ticketActual.precioTotal =
+        ticketActual.precio - ticketActual.cantidadDescuento;
     } else {
-      descuento = false
-      ticketActual.precioTotal = ticketActual.precio
+      descuento = false;
+      ticketActual.precioTotal = ticketActual.precio;
     }
-   listadoHtml += `
+    listadoHtml += `
         <div class="producto-unico">
           <h3>${producto.name}</h3>
           <button data-btnremover="${producto.id}" class="btn-quitar">Remover</button>
           <p>${producto.price}</p>
         </div>
-   `
-   
-  } )
+   `;
+  });
   const html = `
       <h3>Su pedido</h3>
       
@@ -108,28 +131,23 @@ function getTicketHtml(data) {
             <h3>Precio Total:</h3>
             <p>${ticketActual.precioTotal}</p>
           </div>
-          <button class="btn-pedido">Completar pedido</button>
+          <button id="btn-pedido" class="btn-pedido">Completar pedido</button>
       </div>
-  `
-  return html
+  `;
+  return html;
 }
-
-
-
 
 function renderProductos() {
-  document.querySelector('#productos').innerHTML = getProductosHtml(dataArray)
+  document.querySelector("#productos").innerHTML = getProductosHtml(dataArray);
 }
 
-renderProductos()
-
+renderProductos();
 
 function renderTicket() {
-  document.querySelector('#ticket').innerHTML = getTicketHtml(ticketArray)
-  const descuentoParrafo = document.querySelector('#des-disponible')
+  document.querySelector("#ticket").innerHTML = getTicketHtml(ticketArray);
+  const descuentoParrafo = document.querySelector("#des-disponible");
 
-if(descuento) {
-  descuentoParrafo.classList.remove('no-disponible')
-} else if(!descuento)
-descuentoParrafo.classList.add('no-disponible')
+  if (descuento) {
+    descuentoParrafo.classList.remove("no-disponible");
+  } else if (!descuento) descuentoParrafo.classList.add("no-disponible");
 }
